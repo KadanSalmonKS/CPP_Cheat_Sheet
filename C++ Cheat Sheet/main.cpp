7de48e67c8d89346910215ad2dc6431bc7f663cb
Kadian Salmon <kadan_salmon@yahoo.com>
2022-02-23 14:22:01 -0300

    Initial Commit

---
+++ C++ Cheat Sheet/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  C++ Cheat Sheet
+//
+//  Created by Kadian Salmon on 2/23/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
