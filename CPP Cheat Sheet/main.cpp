2b4ebf1213bf509ae85351c4ab4e080ac7a4489b
Kadian Salmon <kadan_salmon@yahoo.com>
2022-09-04 15:01:35 -0300

    Sonarlint for safer development

--- CPP Cheat Sheet/main.cpp
+++ CPP Cheat Sheet/main.cpp
@@ -10,53 +10,72 @@
 #include <fstream>
 #include <string>
 #include <utility>
+#include <cmath>
 #include "main.h"
 #include <time.h>
+#include <set>
 
 using namespace std;
 
 
-void __template__ (void);
+void __template_block__ (void);
 
-void func_enum(void);
+void func_enum(void); //enumeration demonstration
 void func_io_manipulation(void);
 void func_file_read (void);
 void func_file_write (void);
-void func_string_manip (void);
-
+void func_string_manipulation (void);
+size_t get_size (const std::string &); //declaring functions with constant parameter
+void find(void);
 
 class Student
 {
     string name = nullptr;
-    int id;
-    int gradDate;
+    //int id;
+    //int gradDate;
     
 public:
     //void setName(string name);
-    string getName();
+    string getName() const;
     
-    void setName(string nameIn){
+    void setName(const string &nameIn){
         //name = nameIn;
-        this->name = string();
+        this->name = nameIn;
     }
 };
 
 /*void Student::setName(string nameIn){
-    //name = nameIn;
+    --name = nameIn;
     this->name = nameIn;
 }*/
-string Student::getName(){
+
+string Student::getName() const {
+    //functions that don't mutate data must be declared as const
     return name;
 }
 
 
 
+
 int main(int argc, const char * argv[]) {
     
-    int d = 10;
-    Student str1;
-    
-    
+    //int d = 10;
+    //Student str1;
+
+
+
+    //M_PI = "3.14159265358979323846"
+
+    //double x = std::acos(-1);
+    //printf("PI = %lf\n", x);
+    //cout << std::setprecision(20);
+    //cout << "PI = " << x;
+
+    //std::string n = "kadian";
+
+    //cout << get_size(n);
+    //cout << get_size("kadian");
+
     
     //st1 = *new Student();
     
@@ -67,27 +86,32 @@ int main(int argc, const char * argv[]) {
     //func_string_manip();
     
     //str1.setName("Kadian Salmon");
-    str1.setName("Kevin Smith");
+    //str1.setName("Kevin Smith");
 
-    std::cout << str1.getName() << std::endl;
-    
+    //std::cout << str1.getName() << std::endl;
+
+    //find();
+
+    func_enum();
     
     return 0;
 }
 
 
-void __template__(){
+void __template_block__(){
     
     std::cout << "Hello Kadian" << std::endl;
     
 }
 
 void func_enum(){
-    enum Month {Jan = 1, Feb, Mar, Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec};
+    //"enum class Month" must be used instead of "enum Month"
+    enum class Month {Jan = 1, Feb, Mar, Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec};
+
+    //This is to ensure the scoped use of "Month" i.e "Month::Aug" instead of "Aug"
+    Month myBirthMonth = Month::Aug;
     
-    Month myBirthMonth = Aug;
-    
-    std::cout << "My Birth Month is : " << myBirthMonth << std::endl;
+    std::cout << "My Birth Month is : " << static_cast<int>(myBirthMonth) << std::endl;
 }
 void func_io_manipulation(){
     
@@ -99,8 +123,10 @@ void func_file_read(){
     
     fstream fs;
     string line;
-    char *ch = (char *) malloc(sizeof(char) * 200);
-    
+    //auto *ch = (char *) malloc(sizeof(char) * 200);
+    auto ch = std::make_unique<char>(200);
+
+
     fs.open("log.txt", fstream::in);
     
     if (fs.is_open()) {
@@ -112,8 +138,8 @@ void func_file_read(){
         
         /*while (!fs.eof()) {
             fs.read(ch, sizeof(ch));
-            //fs >> ch;
-            //fs.getline(ch,10);
+            --fs >> ch;
+            --fs.getline(ch,10);
             
             line.append(ch);
             
@@ -151,9 +177,26 @@ void func_file_write (){
     
     fs.close();
 }
-void func_string_manip(){
+void func_string_manipulation(){
     
     std::cout << "Hello Kadian 3" << std::endl;
     
+}
+size_t get_size (const std::string &s){
+    return s.size();
+}
+void find(void){
+    std::set<std::string> m = {"Dory","Marlin","Nemo","Emo"};
+
+    const std::string lookup = "Nemo";
+
+
+
+    auto it = m.find("Nemo");
+
+    std::cout << it->data();
+
+
+
 }
 
